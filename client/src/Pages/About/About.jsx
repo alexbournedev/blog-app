739209a8be63b0bb43c708d51b7819d12744be1a
wreed3rd <wreed3rd@gmail.com>
2022-05-19 00:18:17 -0400

    CSS updates

--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -4,7 +4,10 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      
+      <div className="about-container">
+        <div className="about-title">About</div>
+        
+      </div>
     </div>
   )
 }
